05d187e99389c7e722d6ba35ef8224ec2dcf4902
isahamawan <isahamawan@gmail.com>
2022-02-22 00:26:29 +0900

    あと表示モード変更の実装だけ

--- docs/js_for_web/file_menu.js
+++ docs/js_for_web/file_menu.js
@@ -89,6 +89,140 @@ function save_as(evt) {
 };
 
 
+// 表示モードの変更------------------------------------------------------------------------------
+
+// フォントの変更------------------------------------------------------------------------------
+function font_select() {
+
+    let font_kind = font_select_ele.selectedIndex;
+
+    // id属性からHTML要素を取得
+    let font_css_link = document.getElementById("font_css");
+
+    //fontリンクのhref書き換え
+    if (font_kind == 0) {
+        font_css_link.href = "css/font/font_modern_os.css";
+    } else if (font_kind == 2) {
+        font_css_link.href = "css/font/font_simplem.css";
+    } else if (font_kind == 3) {
+        font_css_link.href = "css/font/font_myricam.css";
+    } else if (font_kind == 4) {
+        font_css_link.href = "css/font/font_retro_rpg.css";
+    } else if (font_kind == "beautiful_pixel") {
+        font_css_link.href = "css/font/font_pixel.css";
+    } else if (font_kind == 5) {
+        font_css_link.href = "css/font/font_beautiful_mincho.css";
+    } else if (font_kind == 6) {
+        font_css_link.href = "css/font/font_ud_gothic.css";
+    } else if (font_kind == 7) {
+        font_css_link.href = "css/font/font_ud_mincho.css";
+    } else if (font_kind == 1) {
+        font_css_link.href = "css/font/font_modern_browser.css";
+    }
+
+    //カーソルのサイズをリセット
+    easyMDE.codemirror.execCommand('selectAll');
+    setTimeout(function () {
+        easyMDE.codemirror.setCursor({ line: 0, ch: 0 });
+    }, 10);
+
+}
+
+let font_select_ele = document.getElementById('font');
+font_select_ele.addEventListener('change', font_select);
+
+
+
+// h1で改ページオプション------------------------------------------------------------------------------
+
+window.flg_no_page_break_h1_in_note = false;
+
+function toggle_h1_page_break() {
+
+    flg_no_page_break_h1_in_note = !flg_no_page_break_h1_in_note
+
+    if (flg_no_page_break_h1_in_note == true) {
+        document.getElementsByClassName("CodeMirror-wrap")[0].classList.add("no_page_break_h1");
+    } else {
+        document.getElementsByClassName("CodeMirror-wrap")[0].classList.remove("no_page_break_h1");
+    }
+}
+
+let h1_page_break_cbox = document.getElementById('h1_page_break');
+h1_page_break_cbox.addEventListener('change', toggle_h1_page_break);
+
+
+
+
+
+
+// hrで改ページオプション------------------------------------------------------------------------------
+
+window.flg_page_break_hr_in_note = false;
+
+function toggle_hr_page_break() {
+
+    flg_page_break_hr_in_note = !flg_page_break_hr_in_note
+
+    if (flg_page_break_hr_in_note == true) {
+        document.getElementsByClassName("EasyMDEContainer")[0].classList.add("page_break_hr");
+    } else {
+        document.getElementsByClassName("EasyMDEContainer")[0].classList.remove("page_break_hr");
+    }
+
+}
+
+let hr_page_break_cbox = document.getElementById('hr_page_break');
+hr_page_break_cbox.addEventListener('change', toggle_hr_page_break);
+
+
+
+
+
+
+// 行間を広くするオプション------------------------------------------------------------------------------
+
+window.flg_line_height_wide = false;
+
+function toggle_wide_row() {
+
+    flg_line_height_wide = !flg_line_height_wide
+
+    if (flg_line_height_wide == true) {
+        document.getElementsByClassName("EasyMDEContainer")[0].classList.add("line_height_wide");
+    } else {
+        document.getElementsByClassName("EasyMDEContainer")[0].classList.remove("line_height_wide");
+    }
+
+
+    //カーソルのサイズをリセット
+    easyMDE.codemirror.execCommand('selectAll');
+    setTimeout(function () {
+        easyMDE.codemirror.setCursor({ line: 0, ch: 0 });
+    }, 10);
+}
+
+let wide_row_cbox = document.getElementById('wide_row');
+wide_row_cbox.addEventListener('change', toggle_wide_row);
+
+
+
+
+
+
+// ウィンドウモードスライドショーオプション------------------------------------------------------------------------------
+
+function toggle_window_slide_show() {
+
+    flg_slideshow_fullscreen = !flg_slideshow_fullscreen
+
+}
+
+let window_slide_show_cbox = document.getElementById('window_slide_show');
+window_slide_show_cbox.addEventListener('change', toggle_window_slide_show);
+
+
+
 
 
 // フォントサイズの統一オプション------------------------------------------------------------------------------
@@ -97,23 +231,24 @@ function save_as(evt) {
 window.flg_same_font_size = true;
 document.getElementsByClassName("EasyMDEContainer")[0].classList.add("same_font_size");
 
-/*
-ipcRenderer.on('toggle_same_font_size_from_main', async (event) => {
-
+function toggle_same_font_size() {
     flg_same_font_size = !flg_same_font_size
 
     if (flg_same_font_size == true) {
-        await document.getElementsByClassName("EasyMDEContainer")[0].classList.add("same_font_size");
+        document.getElementsByClassName("EasyMDEContainer")[0].classList.add("same_font_size");
     } else {
-        await document.getElementsByClassName("EasyMDEContainer")[0].classList.remove("same_font_size");
+        document.getElementsByClassName("EasyMDEContainer")[0].classList.remove("same_font_size");
     }
 
 
     //カーソルのサイズをリセット
-    await easyMDE.codemirror.execCommand('selectAll');
-    await setTimeout(function () {
+    easyMDE.codemirror.execCommand('selectAll');
+    setTimeout(function () {
         easyMDE.codemirror.setCursor({ line: 0, ch: 0 });
     }, 10);
+}
 
-});
-*/
+let same_font_size_cbox = document.getElementById('same_font_size');
+same_font_size_cbox.addEventListener('change', toggle_same_font_size);
+
+
