5e2b9c5957b19d89d238de2415713a66541359c7
isahamawan <isahamawan@gmail.com>
2022-03-06 23:31:41 +0900

    aa

---
+++ docs/js/gdrive_hundle.js
@@ -0,0 +1,18 @@
+
+
+Gdfs.loadApi(key_config.api_account, key_config.api_key);
+let gdrive_instance = new Gdfs();
+
+/*
+    gdrive_instance.mkdir(markdown_simplem);
+    gdrive_instance.writeFile("simplem/test.md", "text/plain", easyMDE.value());
+
+
+    gdrive_instance.readFile("simplem/tesw.md").then(
+        function (value) {
+
+            // ここでプロミスオブジェクトの中身をああだこうだする。
+            //console.log(value);
+            easyMDE.value(value);
+        });
+*/
