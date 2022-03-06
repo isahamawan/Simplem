1c009a77286c28c78b3e7932cf0aa6c9221fcbec
isahamawan <isahamawan@gmail.com>
2022-03-06 23:17:12 +0900

    config

---
+++ docs/js_for_web/key_config.js
@@ -0,0 +1,7 @@
+
+
+//リファラーで制限
+const key_config = {
+    api_account: "328742933346-q5scr8j0u27ov1peuti3lk42d6dmq6nu.apps.googleusercontent.com",
+    api_key: "AIzaSyACT6jPq_1wbUKBFfvacukymeRJSEtkX50"
+}
