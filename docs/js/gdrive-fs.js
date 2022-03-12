d9d4865716edfabb4f00ad098a1e7bc620a6c064
isahamawan <isahamawan@gmail.com>
2022-03-12 12:08:36 +0900

    dd

--- docs/js/gdrive-fs.js
+++ docs/js/gdrive-fs.js
@@ -1304,7 +1304,7 @@ parcelRequire = (function (modules, cache, entry, globalName) {
                                 clientSecret,
                                 discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"],
                                 //scope: ["https://www.googleapis.com/auth/drive", "https://www.googleapis.com/auth/drive.appdata", "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive.metadata", "https://www.googleapis.com/auth/drive.metadata.readonly", "https://www.googleapis.com/auth/drive.photos.readonly", "https://www.googleapis.com/auth/drive.readonly"].join(" ")
-                                scope: ["https://www.googleapis.com/auth/drive.file"].join(" ")
+                                scope: ["https://www.googleapis.com/auth/drive.file"].join(" ")//simplem
                             });
                         }
 
