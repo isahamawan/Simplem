b921c02926a19f2528253ca2fc359e2df5d9e896
isahamawan <isahamawan@gmail.com>
2022-03-13 00:52:34 +0900

    tes

--- docs/js/gdrive_hundle.js
+++ docs/js/gdrive_hundle.js
@@ -4,13 +4,42 @@ Gdfs.loadApi(key_config.api_account, key_config.api_key);
 let gdrive_instance = new Gdfs();
 
 async function gdrive_init_for_simplem() {
-    await sleep(5000);
-    await gdrive_instance.mkdir("Simplem");
-    await gdrive_instance.chdir("Simplem");
+
+    //gapiロード終わって、定義成功するまでリトライ処理
+    let execed = false;
+    let count = 0;
+    const maxTries = 50;
+    while (execed === false) {
+        try {
+            execed = true;
+            // リクエスト処理
+
+            await sleep(1000);
+            await gdrive_instance.mkdir("Simplem");
+            await gdrive_instance.chdir("Simplem");
+
+            console.log("gdrive_init_for_simplem : complete");
+
+        } catch (err) {
+            count = count + 1;
+            execed = false;
+
+            console.log("gdrive_init_for_simplem : retry");
+            // エラーハンドリング
+            if (count === maxTries) {
+                execed = true;
+                console.log("gdrive_init_for_simplem : time out (50 tried)");
+            }
+        }
+    }
+
+
     window.simplem_folder_id = gdrive_instance.getCurrentFolderId();
 }
 
 gdrive_init_for_simplem();
+
+
 
 /*
 gdrive_instance.mkdir("Simplem");
