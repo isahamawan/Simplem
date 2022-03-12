72c05c96e4945258eb4765beda09c7b60502c4a1
isahamawan <isahamawan@gmail.com>
2022-03-13 00:17:09 +0900

    tes

--- docs/js/gdrive_hundle.js
+++ docs/js/gdrive_hundle.js
@@ -3,10 +3,12 @@
 Gdfs.loadApi(key_config.api_account, key_config.api_key);
 let gdrive_instance = new Gdfs();
 
-//gdrive_instance.mkdir("Simplem");
-//gdrive_instance.chdir("Simplem");
-window.simplem_folder_id = gdrive_instance.getCurrentFolderId();
-
+async function gdrive_init_for_simplem() {
+    await sleep(1000);
+    await gdrive_instance.mkdir("Simplem");
+    await gdrive_instance.chdir("Simplem");
+    window.simplem_folder_id = gdrive_instance.getCurrentFolderId();
+}
 
 /*
 gdrive_instance.mkdir("Simplem");
