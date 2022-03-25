d38f4100383b5753457a6d63e27e687b2b60ee72
isahamawan <isahamawan@gmail.com>
2022-03-25 15:01:42 +0900

    tes

---
+++ docs/js_for_web/save_as_html_with_toc.js
@@ -0,0 +1,7 @@
+const start_to_above_title = '<!doctype html>< html lang = "ja" ><head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"><!-- title --><title>';
+
+const under_title_to_above_toc
+
+const under_toc_to_above_preview
+
+const under_preview_to_end
