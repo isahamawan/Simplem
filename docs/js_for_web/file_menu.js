d8b6c51d5af61a832baea5168a077c2558e22798
isahamawan <isahamawan@gmail.com>
2022-03-25 13:14:34 +0900

    てｓ

--- docs/js_for_web/file_menu.js
+++ docs/js_for_web/file_menu.js
@@ -840,3 +840,9 @@ function toggle_speech_2x() {
 let speech_2x_cbox = document.getElementById('speech_2x');
 speech_2x_cbox.addEventListener('change', toggle_speech_2x);
 
+
+
+
+
+// 目次付きhtmlファイルとしてPCへ保存------------------------------------------------------------------------------
+//長いのでsave_as_html_with_toc.jsに記述
